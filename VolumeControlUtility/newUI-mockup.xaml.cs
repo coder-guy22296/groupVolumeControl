1a7cc47701713a11d5ec247ef0a2700178bff545
Can Yildirim <canpy30@gmail.com>
2016-06-11 01:21:18 -0700

    working on a UI update

---
+++ VolumeControlUtility/newUI-mockup.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VolumeControlUtility
+{
+    /// <summary>
+    /// Interaction logic for newUI_mockup.xaml
+    /// </summary>
+    public partial class newUI_mockup : UserControl
+    {
+        public newUI_mockup()
+        {
+            InitializeComponent();
+        }
+    }
+}
